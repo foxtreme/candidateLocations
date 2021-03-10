f73d9838076f993afc005856ce8cedbf9dbe9de0
ccharry <chriseth@gmail.com>
2021-03-09 21:59:55 -0500

    added null universities to cities

--- candidateLocationsAPI/restApi/models.py
+++ candidateLocationsAPI/restApi/models.py
@@ -16,7 +16,7 @@ class City(models.Model):
     name = models.CharField(max_length=60)
     province = models.CharField(max_length=60)
     country = models.CharField(max_length=60)
-    universities = models.ManyToManyField(University, null=True)
+    universities = models.ManyToManyField(University, blank=True)
 
     def __str__(self):
         return self.name
